4a2ad3cceede4c22acafcea23fe9c0841eedd61d
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-11-21 17:23:31 -0800

    lualine

--- lua/lualine/themes/onedark_vivid.lua
+++ lua/lualine/themes/onedark_vivid.lua
@@ -4,14 +4,14 @@ local config = require("onedarkpro.config").config
 local onedarkpro = {}
 
 onedarkpro.normal = {
-    a = { bg = colors.green, fg = colors.bg },
-    b = { bg = colors.fg_gutter, fg = colors.green },
+    a = { bg = colors.red, fg = colors.bg },
+    b = { bg = colors.fg_gutter, fg = colors.red },
     c = { bg = colors.bg_statusline, fg = colors.fg },
 }
 
 onedarkpro.insert = {
-    a = { bg = colors.blue, fg = colors.bg },
-    b = { bg = colors.fg_gutter, fg = colors.blue },
+    a = { bg = colors.cyan, fg = colors.bg },
+    b = { bg = colors.fg_gutter, fg = colors.cyan },
 }
 
 onedarkpro.command = {
@@ -25,8 +25,8 @@ onedarkpro.visual = {
 }
 
 onedarkpro.replace = {
-    a = { bg = colors.red, fg = colors.bg },
-    b = { bg = colors.fg_gutter, fg = colors.red },
+    a = { bg = colors.cyan, fg = colors.bg },
+    b = { bg = colors.fg_gutter, fg = colors.cyan },
 }
 
 local inactive_bg = config.options.highlight_inactive_windows and colors.color_column or colors.bg
