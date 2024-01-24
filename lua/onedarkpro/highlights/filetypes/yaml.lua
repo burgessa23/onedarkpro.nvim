548a78351896b06cf3453b671cd5e90c351b915e
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2024-01-23 21:57:55 -0800

    color

--- lua/onedarkpro/highlights/filetypes/yaml.lua
+++ lua/onedarkpro/highlights/filetypes/yaml.lua
@@ -9,7 +9,7 @@ function M.groups(theme)
         ["@function.call.yaml"] = { fg = theme.palette.green },
         ["@punctuation.bracket.yaml"] = { fg = theme.palette.orange },
         ["@parameter.yaml"] = { fg = theme.palette.cyan },
-        ["@string.yaml"] = { fg = theme.palette.yellow },
+        ["@string.yaml"] = { fg = theme.palette.gray },
     }
 end
 
