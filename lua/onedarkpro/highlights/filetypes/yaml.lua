994da17ed64bc073726cd104020541063a5c7603
Andrew Burgess <andrew.burgess@mac.com>
2024-01-23 21:43:41 -0800

    Update yaml.lua

--- lua/onedarkpro/highlights/filetypes/yaml.lua
+++ lua/onedarkpro/highlights/filetypes/yaml.lua
@@ -8,8 +8,8 @@ function M.groups(theme)
         ["@field.yaml"] = { fg = theme.palette.red },
         ["@function.call.yaml"] = { fg = theme.palette.green },
         ["@punctuation.bracket.yaml"] = { fg = theme.palette.orange },
-        ["@parameter.yaml"] = { fg = theme.palette.green },
-        ["@string.yaml"] = { fg = theme.palette.green },
+        ["@parameter.yaml"] = { fg = theme.palette.cyan },
+        ["@string.yaml"] = { fg = theme.palette.yellow },
     }
 end
 
