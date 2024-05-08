9e4c640447d54b0b9659e234007fd537da04b28f
burgessa23 <andrew.burgess@mac.com>
2024-05-07 18:48:19 -0700

    maybe now

--- lua/onedarkpro/highlights/filetypes/norg.lua
+++ lua/onedarkpro/highlights/filetypes/norg.lua
@@ -7,12 +7,12 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-      [ "NeorgH1" ] = { fg = theme.palette.green },
-      [ "NeorgH2" ] = { fg = theme.palette.yellow },
-      [ "NeorgH3" ] = { fg = theme.palette.cyan },
-      [ "NeorgH4" ] = { fg = theme.palette.thm_gld },
-      [ "NeorgH5" ] = { fg = theme.palette.red },
-      [ "NeorgH6" ] = { fg = theme.palette.blue },
+      [ "@NeorgH1" ] = { fg = theme.palette.green },
+      [ "@NeorgH2" ] = { fg = theme.palette.yellow },
+      [ "@NeorgH3" ] = { fg = theme.palette.cyan },
+      [ "@NeorgH4" ] = { fg = theme.palette.thm_gld },
+      [ "@NeorgH5" ] = { fg = theme.palette.red },
+      [ "@NeorgH6" ] = { fg = theme.palette.blue },
     }
 end
 
