0dcbcf8bef96aa6088c610c7525a1ecdffe99ceb
burgessa23 <andrew.burgess@mac.com>
2024-05-07 19:21:11 -0700

    yay

--- lua/onedarkpro/highlights/filetypes/norg.lua
+++ lua/onedarkpro/highlights/filetypes/norg.lua
@@ -7,13 +7,20 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-      [ "@attribute" ] = { fg = theme.palette.green },
-      [ "@neorg.headings.1.prefix.norg" ] = { fg = theme.palette.green },
+      [ "@NeorgH1" ] = { fg = theme.palette.green },
       [ "@NeorgH2" ] = { fg = theme.palette.yellow },
       [ "@NeorgH3" ] = { fg = theme.palette.cyan },
       [ "@NeorgH4" ] = { fg = theme.palette.thm_gld },
       [ "@NeorgH5" ] = { fg = theme.palette.red },
       [ "@NeorgH6" ] = { fg = theme.palette.blue },
+      [ "@undone" ] = { fg = theme.palette.blue },
+      [ "@pending" ] = { fg = theme.palette.yellow },
+      [ "@done" ] = { fg = theme.palette.supergreen },
+      [ "@on_hold" ] = { fg = theme.palette.blue },
+      [ "@urgent" ] = { fg = theme.palette.red },
+      [ "@uncertain" ] = { fg = theme.palette.cyan },
+      [ "@cancelled" ] = { fg = theme.palette.orange },
+      [ "@recurring" ] = { fg = theme.palette.purple },
     }
 end
 
